1f4f793dabf0017732a3b3a8186b5d25df2fe861
Wenceslau Neto <wbaneto@gmail.com>
2024-12-15 13:50:02 +0000

    feat: last commit missed to add VeiculoTipo enum with vehicle type mapping.
    Introduces the VeiculoTipo enum to represent different vehicle types (CARRO, MOTO, PICKUP, BIKE) and provide type-specific object instantiation. Also adds a converter method to parse a string into a VeiculoTipo instance, with error handling for invalid input.

---
+++ src/main/java/com/wenceslau/v4/domain/VeiculoTipo.java
@@ -0,0 +1,33 @@
+package com.wenceslau.v4.domain;
+
+import com.wenceslau.v4.domain.veiculos.Bike;
+import com.wenceslau.v4.domain.veiculos.Carro;
+import com.wenceslau.v4.domain.veiculos.Moto;
+import com.wenceslau.v4.domain.veiculos.Pickup;
+
+import java.util.Arrays;
+
+public enum VeiculoTipo {
+    CARRO,
+    MOTO,
+    PICKUP,
+    BIKE;
+
+    public Veiculo getVeiculo() {
+        return switch (this) {
+            case CARRO -> new Carro();
+            case MOTO -> new Moto();
+            case PICKUP -> new Pickup();
+            case BIKE -> new Bike();
+        };
+    }
+
+    public static VeiculoTipo converter(String vehicleType) {
+        try {
+            return VeiculoTipo.valueOf(vehicleType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Tipo veiculo invalido. Deve ser: " + Arrays.toString(VeiculoTipo.values()));
+        }
+    }
+
+}
