36c2c5903b8d99a35e568982543c20320b7693c8
Wenceslau Neto <wbaneto@gmail.com>
2024-12-15 13:51:13 +0000

    fear: add EstacionamentoApp class for vehicle registration logic. Application DDD layer.
    Introduced a new `EstacionamentoApp` class to handle vehicle registration with plate and type details. This integrates with `VeiculoEstacionamento` and `TaxaRepositorio` to manage parking and tax rates dynamically.

---
+++ src/main/java/com/wenceslau/v4/aplicacao/EstacionamentoApp.java
@@ -0,0 +1,33 @@
+package com.wenceslau.v4.aplicacao;
+
+import com.wenceslau.v4.domain.Veiculo;
+import com.wenceslau.v4.domain.VeiculoEstacionamento;
+import com.wenceslau.v4.domain.VeiculoRegistro;
+import com.wenceslau.v4.domain.VeiculoTipo;
+import com.wenceslau.v4.infrastructure.repositories.TaxaRepositorio;
+
+public class EstacionamentoApp {
+
+    private final VeiculoEstacionamento veiculoEstacionamento;
+    private final TaxaRepositorio taxaRepositorio;
+
+    public EstacionamentoApp(VeiculoEstacionamento veiculoEstacionamento, TaxaRepositorio taxaRepositorio) {
+        this.veiculoEstacionamento = veiculoEstacionamento;
+        this.taxaRepositorio = taxaRepositorio;
+    }
+
+    public VeiculoRegistro registrarPlacaVeiculo(String placa, String veiculoTipo) {
+
+        // Convert the string to the VehicleType enum
+        VeiculoTipo tipo = VeiculoTipo.converter(veiculoTipo);
+
+        Veiculo veiculo = tipo.getVeiculo();
+        veiculo.setPlaca(placa);
+
+        double taxa = taxaRepositorio.getRateByType(veiculoTipo);
+        veiculo.setTaxa(taxa);
+
+        return this.veiculoEstacionamento.registrarVeiculo(veiculo);
+    }
+
+}
