73abf86b94dafc1a55d92a9992fb9dcbe9bdf4e1
Wenceslau Neto <wbaneto@gmail.com>
2024-12-15 13:48:37 +0000

    feat: add vehicle classes with payment calculation logic. Enum to create classes instances based on type.
    Introduced `Bike`, `Carro`, `Moto`, and `Pickup` classes, extending the `Veiculo` base class. Each class implements a `valorPagar` method to calculate parking fees based on duration, applying free periods, base rates, or extra charges for additional time. This provides modular support for different vehicle types in the payment system.

---
+++ src/main/java/com/wenceslau/v4/domain/veiculos/Pickup.java
@@ -0,0 +1,21 @@
+package com.wenceslau.v4.domain.veiculos;
+
+import com.wenceslau.v4.domain.Veiculo;
+
+public class Pickup extends Veiculo {
+
+    @Override
+    protected double valorPagar(long minutos) {
+        if (minutos <= 5) {
+            return 0;
+        } else if (minutos <= 60) {
+            return getTaxa();
+        } else {
+            double valorHoraAdicional = 6.0;
+            long horaExtra = (minutos - 60) / 60;
+            double minutoExtra = (minutos - 60) % 60;
+
+            return getTaxa() + (horaExtra * valorHoraAdicional) + (minutoExtra > 0 ? valorHoraAdicional : 0);
+        }
+    }
+}
